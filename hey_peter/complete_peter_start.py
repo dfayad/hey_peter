d89e96450ffa62f2dc1a7cd30ae801025472b6e7
danielkcasey <dkc52@cornell.edu>
2018-09-30 17:18:51 -0400

    started spotify

--- hey_peter/complete_peter_start.py
+++ hey_peter/complete_peter_start.py
@@ -12,6 +12,7 @@ import os
 import subprocess
 
 import datetime
+import spotipy
 
 #for sms
 from six.moves import input
@@ -150,6 +151,21 @@ def timer(length):
 		print(i)
 		time.sleep(1)
 
+def spotify():
+	client_id = "4c47d5777ede4a168dd986dd3027e9d8"
+	secret = "d2dd908118c049b99d989a1c3c380b65"
+	redirect_uri = "http://localhost"
+	scope = "streaming"
+	token = spotipy.util.prompt_for_user_token('dkc52', scope, client_id, secret, redirect_uri)
+	sp = spotipy.Spotify(auth=token)
+	q = "artist:drake"
+	result = sp.search(q)
+	track = result['items'][0]['track']
+	devices = sp.devices()
+	sp.start_playback(devices[0], uris = [track['uri']])
+
+
+
 while True:
     #loop over speech here
     print("okay im listening")
@@ -188,6 +204,12 @@ while True:
             	l = string_value.split(delimiter=' ')
             	secs = int(l[1])
             	timer(secs)
+
+            elif "play" in value:
+            	print("starting spotify...")
+            	os.system('aplay docs/Sure.wav')
+            	l = str(value).split(delimeter=' ')
+            	spotify(l)
 
             elif ("exit" in value) or ("bye" in value):
                 print("oh ok gbye i'll miss you")
