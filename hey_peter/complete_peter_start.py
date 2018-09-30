251532cf53a278107c35d46a359bdc2332cfaa25
Daniel Fayad <dhf63@corell.edu>
2018-09-29 21:31:59 -0400

    added google voice credentials

--- hey_peter/complete_peter_start.py
+++ hey_peter/complete_peter_start.py
@@ -28,13 +28,13 @@ with m as source: r.adjust_for_ambient_noise(source)
 #send sms
 def send_text():
     voice = Voice()
-    email = 'some_email' #write email here
-    pw = 'some_pw' #write pw here
+    email = 'dfayadv@gmail.com' #write email here
+    pw = 'kiwiabc123.' #write pw here
     voice.login(email, pw)
 
     #phoneNumber = input('Number to send message to: ')
     #text = input('Message text: ')
-    phoneNumber = 1234567890 #write number you wanna get
+    phoneNumber = 6073798229 #write number you wanna get
     text = 'Hello from Hey Peter!'
 
     voice.send_sms(phoneNumber, text)
