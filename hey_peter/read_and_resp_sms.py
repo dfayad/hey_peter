b0f9dcc6359e8a8884d70933df95a5fc0d461734
Daniel Fayad <dhf63@Daniels-MBP-2.home>
2018-10-01 00:07:03 -0400

    added read sms file

---
+++ hey_peter/read_and_resp_sms.py
@@ -0,0 +1,39 @@
+from googlevoice import Voice
+import sys
+from bs4 import BeautifulSoup
+
+def extractsms(htmlsms) :
+    #	Extract all conversations by searching for a DIV with an ID at top level.
+    tree = BeautifulSoup(htmlsms, "lxml")			# parse HTML into tree    class="gc-message-sms-text"
+    texts = tree.find_all("span",attrs={'class' : 'gc-message-sms-text'})
+    #print(type(tree))
+    #print(texts)
+    msgs = []
+    for text in texts:
+        content = text.text.strip()
+        msgs.append(content)
+    #print(msgs)
+    return msgs
+
+voice = Voice()
+email = 'dfayadv@gmail.com'
+pw = 'kiwiabc123.'
+voice.login(email,pw)
+
+voice.sms()
+print("sup")
+
+messages = extractsms(voice.sms.html)
+print("")
+print("")
+if 'groceries' in messages:
+    print("extract groceries")
+    phoneNumber = 6073798229
+    text = 'heres a list of the groceries: [bread, milk]'
+    voice.send_sms(phoneNumber, text)
+else:
+    print("do nothing..")
+
+for message in voice.sms().messages:
+    message.delete()
+print("deleted all messages too btw <3")
