ac00954d64bbdabbfa059184381e16ca98f0f26c
Daniel Fayad <dhf63@corell.edu>
2018-10-02 17:57:33 -0400

    local rpi changes

--- hey_peter/complete_peter_start.py
+++ hey_peter/complete_peter_start.py
@@ -71,7 +71,7 @@ def send_text(voice):
 
     #phoneNumber = input('Number to send message to: ')
     #text = input('Message text: ')
-    phoneNumber = 6073798229 #write number you wanna get
+    phoneNumber = 2019125940 #write number you wanna get
     text = 'Hello from Hey Peter!'
 
     voice.send_sms(phoneNumber, text)
